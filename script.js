47781826c1ed3048aeea14111e14e17c9980558b
Haythem Ben Drissia <BenDrissiaHaythem@gmail.com>
2020-01-29 09:59:08 +0100

    polished and restructured

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// ********** X && Y
-// Returns first value if falsy
-console.log(0 && 5);
+// Grab code snippets from each section in the rendered page to the left
+// paste it in this file to execute
+// click the run button on top to check results in the console to the left
+// Good Luck ✊
 
-// ********** X || Y
-// Returns first value if truthy
-console.log(0 || 5);
+console.log("I've got this 💪");
